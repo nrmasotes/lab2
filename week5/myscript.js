168fc8a16fa9fe6f9135565f702b4e2bede0bb3f
nrmasotes <nrmasotes@student.apc.edu.ph>
2023-01-23 14:57:38 +0800

    My Personal Website - JS Functions

--- week5/myscript.js
+++ week5/myscript.js
@@ -1,6 +1,7 @@
 		// W3 school - JS Confirm Box
 		// W3 school - JS Popup Box
         // contactmentor.com - JS visitor counter
+        // W3 school -JS Functions
         var counterContainer = document.querySelector(".website-counter");
         var resetButton = document.querySelector("#reset");
         var visitCount = localStorage.getItem("page_view");
@@ -15,21 +16,6 @@
         }
         counterContainer.innerHTML = visitCount;
             
-
-        function doDate()
-        {
-            var str = "";
-
-            var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-            var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
-            var now = new Date();
-
-            str += "Today is: " + days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
-            document.getElementById("todaysDate").innerHTML = str;
-        }
-
-setInterval(doDate, 1000);
 
         function myFunction() {
             let text;

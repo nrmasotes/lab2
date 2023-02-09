af68a1a2b1d73fbc6c9f5eb4430b6e535cc6fec1
nrmasotes <nrmasotes@student.apc.edu.ph>
2023-02-09 17:23:51 +0800

    updated guests.php

--- week9/guests.php
+++ week9/guests.php
@@ -16,15 +16,6 @@ if ($conn->connect_error) {
 }
 
 
-// sql to create table
-$sql = "CREATE TABLE nrmasotes_myguests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
-
 
 $sql = "SELECT id, fullName, emailUsed, commentInput FROM nrmasotes_myguests";
 $result = $conn->query($sql);
